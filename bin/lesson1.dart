c1ee5a12c8caee4535ff4eeea03bcd8cab087b60
Dmitry Morozov <9984398@gmail.com>
2024-06-20 21:55:54 +0300

    -> Lesson1

--- bin/lesson1.dart
+++ bin/lesson1.dart
@@ -1,5 +1,16 @@
-import 'package:lesson1/lesson1.dart' as lesson1;
+import 'package:lesson1/lesson1.dart' as l1;
 
 void main(List<String> arguments) {
-  print('Hello world: ${lesson1.calculate()}!');
+  print('Hello world: ${l1.calculate()}!');
+
+  int a = 10;
+  int b = 2;
+
+  print('Dividing = ${l1.divide(a, b)}');
 }
+
+// std::Vector <std::String> arguments;
+// List: arguments
+// main (  int argc, char **argv )
+// int a ( named memory allocation 0xffff43 )
+// python: print(f'Hello world: {l1.calculate()}!')
--- lib/lesson1.dart
+++ lib/lesson1.dart
@@ -1,3 +1,13 @@
 int calculate() {
   return 6 * 7;
 }
+
+int divide(int a, int b) {
+  return a % b;
+  // if (b != null && b != 0) {
+  //   return a % b;
+  // } else {
+  //   print('Invalid arguments!');
+  //   return -1;
+  // }
+}
