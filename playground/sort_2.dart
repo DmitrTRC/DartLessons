89b37cdd6e933d4e84f2aae06d7bef04d1001d80
Dmitry Morozov <9984398@gmail.com>
2024-06-28 21:19:57 +0300

    Issue

--- playground/sort_2.dart
+++ playground/sort_2.dart
@@ -4,6 +4,7 @@ List<int> mysort(List<int> list) {
   for (int i = 0; i < cloned.length; i++) {
     for (int j = 0; j < cloned.length - i - 1; j++) {
       if (cloned[j + 1] < cloned[j]) {
+        // Separate code into Swap() function
         int temp = cloned[j];
         cloned[j] = cloned[j + 1];
         cloned[j + 1] = temp;
