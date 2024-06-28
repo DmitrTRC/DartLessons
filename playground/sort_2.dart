33b9de2cb712bf669b6e7ab0d30cc8467513cb76
Arisha75144 <88162494+Arisha75144@users.noreply.github.com>
2024-06-28 19:49:38 +0300

    Create sort_2.dart

---
+++ playground/sort_2.dart
@@ -0,0 +1,19 @@
+List<int> mysort(List<int> list) {
+  List<int> cloned = List.from(list);
+
+  for (int i = 0; i < cloned.length; i++) {
+    for (int j = 0; j < cloned.length - i - 1; j++) {
+      if (cloned[j + 1] < cloned[j]) {
+        int temp = cloned[j];
+        cloned[j] = cloned[j + 1];
+        cloned[j + 1] = temp;
+      }
+    }
+  }
+  return cloned;
+}
+
+void main() {
+  List<int> l = [6, 90, 9, 10];
+  print(mysort(l));
+}
