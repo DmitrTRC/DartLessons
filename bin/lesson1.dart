21f53fdd1e9f54dd5d043c9ccdaa935a0e1f54ae
Dmitry Morozov <9984398@gmail.com>
2024-07-08 21:08:40 +0300

    -> Demo record

--- bin/lesson1.dart
+++ bin/lesson1.dart
@@ -3,8 +3,15 @@ import 'package:lesson1/lesson1.dart' as l1;
 void main(List<String> arguments) {
   print('Hello world: ${l1.calculate()}!');
 
-  int a = 10;
-  int b = 2;
+  // int a = 10;
+  // int b = 2;
+
+  String st = 'Just a demo string';
+
+  var t  = ('This is a String', 4, true);
+
+  var (a, b, c) = t;
+  // Equal c++ dynamic_cast<std::tuple<std::string, int, bool>>
 
   print('Dividing = ${l1.divide(a, b)}');
 }
