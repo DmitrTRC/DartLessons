26fc3646b6c04f228e0ff14604d82b1c36db8f17
Dmitry Morozov <9984398@gmail.com>
2024-07-09 21:17:16 +0300

    -> Start testing

--- playground/sort_2.dart
+++ playground/sort_2.dart
@@ -1,4 +1,3 @@
-
 void swap<T>(List<T> list, int index1, int index2) {
   T temp = list[index1];
   list[index1] = list[index2];
@@ -11,7 +10,7 @@ List<T> mysort<T extends Comparable>(List<T> list) {
   for (int i = 0; i < cloned.length; i++) {
     for (int j = 0; j < cloned.length - i - 1; j++) {
       if (cloned[j + 1].compareTo(cloned[j]) < 0) {
-        swap(cloned,j+1,j);
+        swap(cloned, j + 1, j);
       }
     }
   }
