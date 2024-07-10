33b0fa1953265ebdaeb288f3dea63027c2e373b3
Dmitry Morozov <9984398@gmail.com>
2024-07-10 20:51:11 +0300

    -> Project refactor

--- test/insertion_sort_test.dart
+++ test/insertion_sort_test.dart
@@ -1,51 +1,53 @@
-import 'package:lesson1/qsort.dart';
+import 'package:lesson1/sort_2.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('Quicksort', () {
     test('Empty list', () {
       List<int> list = [];
-      expect(quickSortHuge(list), []);
+      expect(mysort(list), []);
     });
 
     test('Single element', () {
       var list = [1];
-      expect(quickSortHuge(list), [1]);
+      expect(mysort(list), [1]);
     });
 
     test('Two elements sorted', () {
       var list = [1, 2];
-      expect(quickSortHuge(list), [1, 2]);
+      expect(mysort(list), [1, 2]);
     });
 
     test('Two elements unsorted', () {
       var list = [2, 1];
-      expect(quickSortHuge(list), [1, 2]);
+      expect(mysort(list), [1, 2]);
     });
 
     test('Multiple elements', () {
       var list = [5, 3, 4, 1, 2];
-      expect(quickSortHuge(list), [1, 2, 3, 4, 5]);
+      expect(mysort(list), [1, 2, 3, 4, 5]);
     });
 
     test('Multiple elements with duplicates', () {
       var list = [5, 3, 4, 1, 2, 5, 1, 2];
-      expect(quickSortHuge(list), [1, 1, 2, 2, 3, 4, 5, 5]);
+      expect(mysort(list), [1, 1, 2, 2, 3, 4, 5, 5]);
     });
 
     test('Big list', () {
       var list = List.generate(10000, (index) => index);
-      expect(quickSortHuge(list), List.generate(10000, (index) => index));
+      expect(mysort(list), List.generate(10000, (index) => index));
     });
 
     test('Medium list', () {
       var list = List.generate(100000, (index) => index);
-      expect(quickSortHuge(list), List.generate(100000, (index) => index));
+      expect(mysort(list), List.generate(100000, (index) => index));
     });
 
     test('Huge list', () {
       var list = List.generate(1000000, (index) => index);
-      expect(quickSortHuge(list), List.generate(1000000, (index) => index));
+      expect(mysort(list), List.generate(1000000, (index) => index));
+
     });
+
   });
 }
